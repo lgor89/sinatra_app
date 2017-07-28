891901aaf8cb86d917736643223b7f7df0fce9ee
lgor89 <igor.penkovsky@gmail.com>
2017-07-28 12:26:25 +0300

    only github auth

--- start.rb
+++ start.rb
@@ -36,60 +36,16 @@ get '/oauth2/callback' do
   )
   access_token = JSON.parse(response)['access_token']
   out = RestClient.get('https://api.github.com/user', params: { access_token: access_token })
-  @login = JSON.parse(out)['login']
-  @user = User.create(name: @login, access_token: access_token)
+  login = JSON.parse(out)['login']
+  @user = User.create(name: login, access_token: access_token)
   crypt_token
   if out.nil?
     redirect to '/'
   else
+    session[:name] = @user.name
+    binding.pry
     redirect to '/user'
   end
-end
-#-----------------------------------------------------------
-get '/signup' do
-  erb :signup
-end
-#-------------------------------------------------------------
-post '/signup' do
-  if params[:password] == params[:password_confirmation]
-    password = params[:password]
-    secure_password = BCrypt::Password.create(password)
-    @user = User.create(name: params[:name], email: params[:email], password: secure_password)
-    crypt_token
-    redirect to '/user'
-  else
-    erb :signup
-  end
-end
-#--------------------------------------------------------
-get '/index' do
-  if current_user?
-    "Welcome #{User.find(session[:id]).name}"
-  else
-    redirect to '/signup'
-  end
-end
-#--------------------------------------------------------
-post '/login' do
-  email = params[:email]
-  @user = User.find_by(email: email)
-  if @user.nil?
-    redirect to '/signup'
-  else
-    user_password = @user.password
-    encrypt_user_password = BCrypt::Password.new(user_password)
-    if encrypt_user_password == params[:password]
-      @login = @user.name
-      crypt_token
-      redirect to '/user'
-    else
-      redirect to '/login'
-    end
-  end
-end
-#----------------------------------------------------------
-get '/login' do
-  erb :login
 end
 #--------------------------------------------------------
 get '/user' do
@@ -97,14 +53,14 @@ get '/user' do
     @login = session[:name]
     erb :user
   else
-    redirect to '/login'
+    redirect to '/'
   end
 end
 #-----------------------------------------------------------
 get '/logout' do
   session[:token] = nil
   session[:id] = nil
-  redirect to '/signup'
+  redirect to '/'
 end
 def current_user?
   if session[:token].nil?
